f185a4dc3b35630f36f8d11829b7461c2d5be5a9
Artem Naluzhnyy <Artem.Naluzhnyy@gmail.com>
2023-07-25 17:05:29 +0200

    linter breaks on the comment?

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <!-- <img src={logo} className="App-logo" alt="logo" /> -->
         <p>
           Онлайн тест соціально-політичних вподобань «Обирай!» наразі знаходиться в розробці.
         </p>
