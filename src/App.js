765dd29c4c6708cbdd653fc41cfcf7c828d24367
Artem Naluzhnyy <Artem.Naluzhnyy@gmail.com>
2023-07-25 17:00:58 +0200

    Add "dev mode" project status with link to our Wiki

--- src/App.js
+++ src/App.js
@@ -5,17 +5,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <!-- <img src={logo} className="App-logo" alt="logo" /> -->
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Онлайн тест соціально-політичних вподобань «Обирай!» наразі знаходиться в розробці.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/obyrai/obyrai-survey/wiki"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Дізнайтеся більше про наш проект
         </a>
       </header>
     </div>
